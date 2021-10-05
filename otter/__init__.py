e628fea708301432c294d26730a6c5189c1bb437
volbil <volbilnexus@gmail.com>
2021-10-05 23:57:19 +0900

    Updated version

--- otter/__init__.py
+++ otter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0"
+__version__ = "1.1"
 
 from .core import Otter, Blueprint
 from .client import Client
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="otterpy",
-    version="1.0",
+    version="1.1",
     author="Volodymyr Biloshytskyi",
     author_email="volbil@protonmail.com",
     description="Otter - framework for microservices.",
